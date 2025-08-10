3a6df6d1d9b0e0da00a55f900ba575c47d9c860d
Serhii Holenkov <holenkov.s.s@gmail.com>
2025-08-10 13:54:24 +0300

    solution

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,24 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+public class EventManager {
+    private List<EventListener> listeners = new CopyOnWriteArrayList<>();
+
+    public void registerListener(EventListener listener) {
+        listeners.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        listeners.forEach(e -> e.onEvent(event));
     }
 
     public void shutdown() {
-
+        listeners.clear();
     }
 }
